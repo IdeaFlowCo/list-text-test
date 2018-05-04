7112e72ba9955d57d2cfafba11e65f406f1814ec
Albert Siddhartha Slawinski <errendir@gmail.com>
2018-05-04 22:04:31 +0200

    Missing files, use context api

---
+++ global.d.ts
@@ -0,0 +1,14 @@
+declare module "*.styl" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
+
+declare module "linked-map" {
+  const value: any;
+  export default value;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,27 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
+import styles from './index.styl'
+
+const { Consumer: AppStateConsumer, Provider: AppStateProvider } = React.createContext({ ideas: [] })
+
+class ComplexComponent extends React.Component<{ ideas: string[] }> {
+  render() {
+    return <>
+      There are {this.props.ideas.length} ideas!
+    </>
+  }
+}
+
 ReactDOM.render(
-  <div>Hello</div>,
+  <AppStateProvider value={{ ideas: ["a", "b"] }}>
+    <div className={styles.mainBox} style={{ fontSize: 12 }}>Hello</div>
+    <div className={styles.mainBox} style={{ fontSize: 16 }}>
+      Hello:&nbsp;
+      <AppStateConsumer>
+        {(value) => <ComplexComponent ideas={value.ideas} />}
+      </AppStateConsumer>
+    </div>
+  </AppStateProvider>,
   document.querySelector("#reactAppMountPoint")
 )
