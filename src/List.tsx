f1d8948595d980d5780cf4412caa8e6cf7d58202
Albert Siddhartha Slawinski <errendir@gmail.com>
2018-05-04 23:02:02 +0200

    Move caret when it goes out of bounds

--- src/List.tsx
+++ src/List.tsx
@@ -6,23 +6,64 @@ import { withStateSelector } from './AppState'
 import styles from './List.styl'
 
 class Item extends React.Component<any> {
+  private ref: React.RefObject<HTMLDivElement> = React.createRef()
+
   render() {
-    return <div 
+    return <div
+      ref={this.ref}
       className={styles.listItem}
       contentEditable
       dangerouslySetInnerHTML={{ __html: he.encode(this.props.idea) }}
+
+      onKeyDown={this.onKeyDown}
     />
+  }
+
+  onKeyDown = (event: React.KeyboardEvent<any>) => {
+    const { key } = event
+    if (key === "ArrowUp") {
+      this.props.caretExit(this.props.index, -1)
+    } else if (key === "ArrowDown") {
+      this.props.caretExit(this.props.index, +1)
+    }
+  }
+
+  focusEnd = () => {
+    this.ref.current.focus()
+    const selection = document.getSelection()
+
+    const node = this.ref.current.childNodes[0]
+    console.log({ node })
+    const range = new Range()
+    range.selectNodeContents(node)
+
+    selection.removeAllRanges()
+    selection.addRange(range)
   }
 }
 
 class List extends React.Component<any> {
+  private itemInstances: Item[] = []
+  
   render() {
     return <>
       {this.props.ideas.map((idea, i) => {
-        return <Item key={i} idea={idea} />
+        return <Item
+          ref={(item) => this.itemInstances[i] = item}
+          key={i}
+          index={i}
+          idea={idea}
+          caretExit={this.caretExit} />
       })}
       <button onClick={() => this.props.addNewIdea("New idea")}>Add new idea</button>
     </>
+  }
+
+  caretExit = (itemIndex: number, direction: 1 | -1) => {
+    const instance = this.itemInstances[itemIndex + direction]
+    if (instance) { 
+      instance.focusEnd()
+    }
   }
 }
 
