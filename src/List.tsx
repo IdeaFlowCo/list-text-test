542422a25a88fe0081d96c8bc0720c232bc4edd5
Albert Siddhartha Slawinski <errendir@gmail.com>
2018-05-05 00:09:53 +0200

    idea splitting and better caret offset processing

--- src/List.tsx
+++ src/List.tsx
@@ -5,34 +5,89 @@ import { withStateSelector } from './AppState'
 
 import styles from './List.styl'
 
-class Item extends React.Component<any> {
+class Item extends React.Component<any, any> {
   private ref: React.RefObject<HTMLDivElement> = React.createRef()
 
   render() {
+    const content = this.props.idea
     return <div
       ref={this.ref}
       className={styles.listItem}
       contentEditable
-      dangerouslySetInnerHTML={{ __html: he.encode(this.props.idea) + "\n\n" }}
+      dangerouslySetInnerHTML={{ __html: he.encode(content) + "\n\n" }}
 
       onKeyDown={this.onKeyDown}
+      onInput={this.onChange}
     />
+  }
+
+  private getValue() {
+    return this.ref.current.innerText.replace(/\n\n$/, "")
+  }
+
+  private getCaretOffset() {
+    const range = window.getSelection().getRangeAt(0)
+    let caretOffset = range.endOffset
+    let node = range.endContainer
+    
+    while (node !== this.ref.current) {
+      const siblingNodes = Array.from(node.parentNode.childNodes)
+      const index = siblingNodes.indexOf(node)
+
+      console.log({
+        index,
+        siblingNodes,
+      })
+
+      for(const childNode of siblingNodes.slice(0, index)) {
+        caretOffset += (childNode.innerText || childNode.textContent || "").length
+      }
+      node = node.parentNode
+    }
+
+    return caretOffset
+  }
+
+  private getTextAroundTheCaret(before: number, after: number) {
+    const offset = this.getCaretOffset()
+    return {
+      offset,
+      text: this.getValue().slice(offset - before, offset + after),
+      before: this.getValue().slice(0, offset - before),
+      after: this.getValue().slice(offset + after),
+    }
   }
 
   onKeyDown = (event: React.KeyboardEvent<any>) => {
     const { key } = event
+    if (key === "Enter") {
+      this.onEnter(event)
+    }
+
     const range = window.getSelection().getRangeAt(0)
-    const node = range.startContainer
-    const textAfterCaret = node.textContent.slice(range.startOffset)
+    const { before, after } = this.getTextAroundTheCaret(0, 0)
+    console.log({ before, after })
 
-    if (!range) return
+    if (!range || !range.collapsed) return
 
-    if (key === "ArrowUp" && range.startOffset === 0 && range.collapsed) {
+    if (key === "ArrowUp" && before === "") {
       this.props.caretExit(this.props.index, -1)
       event.preventDefault()
-    } else if (key === "ArrowDown" && textAfterCaret.match(/^\n?$/) && range.collapsed) {
+    } else if (key === "ArrowDown" && after.match(/^\n?$/)) {
       this.props.caretExit(this.props.index, +1)
       event.preventDefault()
+    }
+  }
+
+  onEnter = (event: React.KeyboardEvent<any>) => {
+    console.log("ENTER")
+    const { text, offset } = this.getTextAroundTheCaret(+1, +1)
+    console.log({ text, offset })
+
+    const match = text.match(/\n/)
+    if (match) {
+      console.log("splitting", this.props.index, offset)
+      this.props.onSplit(this.props.index, offset)
     }
   }
 
@@ -63,17 +118,22 @@ class List extends React.Component<any> {
           key={i}
           index={i}
           idea={idea}
-          caretExit={this.caretExit} />
+          caretExit={this.caretExit}
+          onSplit={this.onSplit}
+        />
       })}
-      <button onClick={() => this.props.addNewIdea("New idea")}>Add new idea</button>
     </>
   }
 
-  caretExit = (itemIndex: number, direction: 1 | -1) => {
+  private caretExit = (itemIndex: number, direction: 1 | -1) => {
     const instance = this.itemInstances[itemIndex + direction]
     if (instance) { 
       instance.focusEnd()
     }
+  }
+
+  private onSplit = (itemIndex: number, offset: number) => {
+    this.props.split(itemIndex, offset)
   }
 }
 
@@ -82,7 +142,17 @@ export default withStateSelector(
   (appState) => ({ ideas: appState.ideas }),
   (dispatch) => ({
     addNewIdea: (idea: string) => dispatch(appState => ({
-      ...appState, ideas: [...appState.ideas, idea]
-    }))
+      ideas: [...appState.ideas, idea]
+    })),
+    split: (itemIndex, offset) => dispatch(appState => {
+      const { ideas } = appState
+      const ideaToSplit = ideas[itemIndex]
+      const newIdea1 = ideaToSplit.slice(0, offset)
+      const newIdea2 = ideaToSplit.slice(offset)
+      return {
+        ideas: [...ideas.slice(0,itemIndex), newIdea1, newIdea2, ...ideas.slice(itemIndex+1)]
+      }
+    })
+
   })
 )
